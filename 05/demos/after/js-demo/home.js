ded8ce8bc5290e870dcddccb447fff4a1670a76f
Sangeeta Devi <sd705194@gmail.com>
2023-04-22 23:39:27 +0530

    single and multi-line comments

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -1,2 +1,4 @@
-showMessage("help Me");
-console.log("Are You Mad?");
+showMessage("help Me");          //this is a single line comment 
+console.log("Are You Aware?");
+/* this is a multiline comment that is we are 
+   adding comments of more than one line */
