de8affd850ddd2d3824a782df1c138fd4b94c91d
Sangeeta Devi <sd705194@gmail.com>
2023-04-26 22:27:47 +0530

    program

---
+++ 02/demos/after/js-demo/programflow.js
@@ -0,0 +1,41 @@
+if(1==1) //true
+{
+console.log("1 is equal to 1");
+}
+if(4>6)  //false
+{
+console.log("4 is greater than 6");
+}
+if(4<6)   //true
+{
+console.log("4 is less than 6");
+}
+if("0")  //"o" is not false but a string
+{
+console.log(54);
+}
+if(0)
+{
+console.log(54);
+}
+if(1.1+1.3!=2.4)
+{
+console.log(true);
+}
+if(+(1.1+1.3).toFixed(2)!==2.4)
+{
+console.log(true);
+}
+var i=10;
+if(i<10)
+{
+    console.log('i is less than 10');
+}
+else if(i>10)
+{
+    console.log('i is greater than 10');
+}
+else
+{
+console.log(i);
+}
