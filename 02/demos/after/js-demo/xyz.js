75fccee11c387f35e0651782541b0fea919ee2df
Sangeeta Devi <sd705194@gmail.com>
2023-04-26 22:45:54 +0530

    initial

--- 02/demos/after/js-demo/xyz.js
+++ 02/demos/after/js-demo/xyz.js
@@ -1,20 +1,20 @@
-var ar=[];
-for(i=1;i<=50;i++)
- {
- ar.push(i)
-}
-console.log(ar)
-var a=ar.filter((i) => ((i%2==0) && (i%5==0)))
-console.log(a); 
-
-
-// function myfun()
-// {
-// let ar=["sourav","Gourav","Rahul","Falgun"];
-// var a=ar.filter((item)=>(item.length>=3))
-// console.log(a);
+// var ar=[];
+// for(i=1;i<=50;i++)
+//  {
+//  ar.push(i)
 // }
-// myfun()
+// console.log(ar)
+// var a=ar.filter((i) => ((i%2==0) && (i%5==0)))
+// console.log(a); 
+
+
+ function myfun()
+{
+let ar=["sourav","Gourav","Ram","Falgun","sa"];
+var a=ar.filter((item)=>(item.length>3))
+console.log(a);
+}
+myfun()
 
 // var ar=[2,5,8,9,10];
 // //var a= ar.find((item)=>(item%2==0))
