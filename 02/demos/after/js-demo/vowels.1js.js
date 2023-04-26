da2f2ba9f637a6e1cdc09e21ec65fe862343d0b7
Sangeeta Devi <sd705194@gmail.com>
2023-04-26 22:33:44 +0530

    vowels

---
+++ 02/demos/after/js-demo/vowels.1js.js
@@ -0,0 +1,24 @@
+var str="How many vowels and consonants in a given sentence";
+var regex=/[aeiou]/;
+var count=0;
+var count1=0;
+for(var i of str)
+{
+if(i.match(regex))
+{
+count++;
+}
+else
+{
+if(i==" ")
+{
+count1=count1;
+}
+else
+{
+count1++;
+}
+}
+}
+console.log("Count of vowels is:",count);
+console.log("Count of consonants is:",count1);
