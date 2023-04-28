a741f5ab050576c606a959d40f6fd08c4c51303a
Sangeeta Devi <sd705194@gmail.com>
2023-04-28 15:54:19 +0530

    initial

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -1,5 +1,31 @@
-let  price="A123.01"
- price=Number.parseFloat(price)
-showMessage(price);   
+// let  price="A123.01"
+//  price=Number.parseFloat(price)
+// showMessage(price);   
 
+// function fn()          // function declaration
+// {
+// console.log("Here I am");
+// }
+// fn();
+                      
 
+//  var f=function()                      //function expression
+//  {
+//     console.log(true);
+//     showMessage(true);
+//  }
+//  f();
+
+// var f=function(a,b)
+// {
+// console.log(a);
+// console.log(b);
+// }
+// f("Harry","Bhai");
+
+ var f=function(value)
+ {
+ var n=value*4;
+ return n;
+ }
+ showMessage(f(2));
