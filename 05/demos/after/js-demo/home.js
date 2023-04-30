df7cc9f3e6ccc396e6aac0304c09f598f0e5e4b6
Sangeeta Devi <sd705194@gmail.com>
2023-04-30 22:21:15 +0530

    initial

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -1,24 +1,5 @@
-// const app=
-// {
-// productId:12345,
-// username:"Joe",
-// orderNumber:789
-// };
-// function showProductId()
-// {
-// console.log(app.productId);
-// console.log(app.username);
-// }
-// showProductId();
-
-let productId=12345;
-function showProduct(id)
-{
-    function fix()
-    {
-    console.log("in fix:",productId);
-    }
-    fix();
-    console.log("in showProduct:",productId);
-}
-showProduct();
+productId=456;               //hoisting
+console.log(productId);
+ var productId=123;
+//let productId=123;
+//const productId=123;
