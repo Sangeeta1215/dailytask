4fe1fda0763c271bb9dd3760bc1e7642f293c126
Sangeeta Devi <sd705194@gmail.com>
2023-04-30 19:40:50 +0530

    initial

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -1,16 +1,43 @@
-// const button=document.getElementById('see-review');
-// button.addEventListener('click',function(){console.log('click');});
+let values=[1,2,3];
+let array=Array.of(1,4,6);       //Array Object with method Of
+console.log(values);
+console.log(array);
+console.log(typeof(values));
+console.log(values[1]);
+values[0]=5;
+console.log(values);
 
-const button=document.getElementById('see-review');
-button.addEventListener('click',function()
-{const review=document.getElementById("review");
-if(review.classList.contains("d-none"))
-{
-review.classList.remove("d-none");
-button.textContent='CLOSE REVIEW';
-}
-else{
-review.classList.add("d-none");
-button.textContent='SEE REVIEW';
-}
-});
+// values.push("a");
+// console.log(values);
+// const last=values.pop();
+//  console.log(values,last);
+// let first=values.shift();
+// console.log(values,first);
+// values.unshift("a");
+// console.log(values);
+
+
+//let val=['s','r','c'];
+// console.log(val.indexOf("c"));
+// console.log(val.indexOf("d"));
+
+// const set=val.filter(function(item)
+// {
+// return item >'c';
+// });
+// console.log(set);
+
+// const set=val.find(function(item)
+// {
+// return item > "c";
+// });
+// console.log(set);
+
+// const values1=['a','b','c','d'];
+// values1.forEach(function(item)
+// {
+// console.log(item);
+// })
+
+
+
