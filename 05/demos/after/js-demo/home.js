b8485e050aa03f378fc0ddf855f715d89e9d6a36
Sangeeta Devi <sd705194@gmail.com>
2023-04-30 12:07:31 +0530

    initial

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -1,49 +1,10 @@
-// let  price="A123.01"
-//  price=Number.parseFloat(price)
-// showMessage(price);   
+let person=
+{
+name:"John",
+age:25,
+partTime:false,
+};
+// person.age=35;
+person["age"]=45;
+showMessage(person.age);
 
-// function fn()          // function declaration
-// {
-// console.log("Here I am");
-// }
-// fn();
-                      
-
-//  var f=function()                      //function expression
-//  {
-//     console.log(true);
-//     showMessage(true);
-//  }
-//  f();
-
-// var f=function(a,b)
-// {
-// console.log(a);
-// console.log(b);
-// }
-// f("Harry","Bhai");
-
-// let key=4;
-//  var f=function(value)
-//  {
-//  let n=value*4*key;
-//  return n;
-//  }
-//  showMessage(f(2));
-//  console.log(f(4));
-//  console.log(n);
-
- let key=42;
- function keyGenerator(value)                   //function scope
- {
-let secretCode=function(){
-    let key=12;
-    console.log("In secret key is:",key);
-    return key;
-}
-let code=value*secretCode();
-console.log("In key generator key is:",key);
-return code;
- }
- showMessage(keyGenerator(2));
- changePercentOff(60);
