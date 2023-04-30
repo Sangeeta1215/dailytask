28a318263d0b4787da9063c4b50c5d904c61619e
Sangeeta Devi <sd705194@gmail.com>
2023-04-30 16:43:14 +0530

    initial

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -1,8 +1,15 @@
-let string="Gourav";
-let number=123;
+// let string="Gourav";
+// let s="  Sourav    ";
+// let number=123;
 //showMessage(string.charAt(3));
 //showMessage(string.toUpperCase());
 //showMessage(string.slice(2,4));
-showMessage(isNaN(number));
-showMessage(isNaN(string));
+// showMessage(s.trim());
+// showMessage(isNaN(number));
 
+// showMessage(isNaN(number));
+// showMessage(isNaN(string));
+
+const header=document.getElementById('message');
+header.style.color='red';
+header.style.fontWeight="1000";
