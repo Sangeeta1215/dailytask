1ca71893bc85dad1ffe0db932cba855e9afc93eb
Sangeeta Devi <sd705194@gmail.com>
2023-04-30 21:51:14 +0530

    initial

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -1,43 +1,10 @@
- //let values=[1,2,3];
-// let array=Array.of(1,4,6);       //Array Object with method Of
-// console.log(values);
-// console.log(array);
-// console.log(typeof(values));
-// console.log(values[1]);
-// values[0]=5;
-// console.log(values);
+// const containers=document.getElementsByClassName("container");
+// containers[2].classList.add('d-none');
+// console.log(containers);
 
-// values.push("a");
-// console.log(values);
-// const last=values.pop();
-//  console.log(values,last);
-// let first=values.shift();
-// console.log(values,first);
-// values.unshift("a");
-// console.log(values);
-
-
-let val=['s','r','c'];
-// console.log(val.indexOf("c"));
-// console.log(val.indexOf("d"));
-
-// const set=val.filter(function(item)
-// {
-// return item >'c';
-// });
-// console.log(set);
-
-// const set=val.find(function(item)
-// {
-// return item > "c";
-// });
-// console.log(set);
-
-const values1=['a','b','c','d'];
-values1.forEach(function(item)
+let productId=12345;
+function showProductId()
 {
-console.log(item);
-})
-
-
-
+console.log(productId);
+}
+showProductId();
