2805356d09ecab3d562268b1f31e76b03304addb
Sangeeta Devi <sd705194@gmail.com>
2023-04-30 23:56:55 +0530

    initial

---
+++ 05/demos/after/js-demo/obj.js
@@ -0,0 +1,32 @@
+// var object=
+// {
+// name:"gourav",
+// age:7,
+// roll:function()
+// {
+// rollno:1;
+// }
+
+// }
+// for(var i in object)
+// {
+// console.log(i + ":"+ object[i]);
+// }
+
+let products=[
+    {
+    productID:680,
+    name:"HL Road Frame - Black,58",
+    productNumber:"FR-R92B-58"
+    },
+    {
+        productID:890,
+        name:"Sport-100 Helmet,Red",
+        productNumber:"HL-U509-R"
+    }
+];
+for(var i of products)
+{
+console.log(JSON.stringify(i));
+}
+console.log(typeof(products));
