aeb9e59a7b91aaeddcd6745728ebff198bf67348
Sangeeta Devi <sd705194@gmail.com>
2023-05-01 17:29:08 +0530

    initial

--- 05/demos/after/js-demo/obj.js
+++ 05/demos/after/js-demo/obj.js
@@ -1,50 +1,46 @@
-// let price=200;
-// if(price>10 && price<1600)
-// {
-// console.log("Both conditions are true:");
-// }
-// if(price>10 && price<200)
-// {
-// console.log("Price is greater than 10 but not less than 200");
-// }
-// if(price>10 || price<1600)
-// {
-//     console.log("Both conditions are true:");  
-// }
-// if(price>10 || price<200)
-// {
-//  console.log("Price is greater than 10 but not less than 200");
-// }
-// if(!(price<10))
-// {
-// console.log("Price is not less than 10");
-// }
-
-function shortCircuitAndSample()
+function tryCatch()
 {
 let result;
-result=isColorRed("Black") && isGreaterThan1400(1400);
-}
-function isColorRed(value)
+try
 {
-console.log("In the isColorRed() function");
-return value === "Red";
+console.log("an error will ocuur");
+result=x/9;
+console.log("an error is occured");
 }
-function isGreaterThan1400(value)
+catch(error)
 {
-console.log("In the isGreaterThan1400() function");
-return value >1400;
+console.log(error.message);             //a javascript error object always has "name" and "message" properties.
 }
-shortCircuitAndSample();
+finally
+{
+console.log("This block always executes");
+}
+}
+tryCatch();
 
-function shortCircuitOrSample()
-{
-let result;
-console.log("");
-console.log("Calling isColorred() first");
-//Each expression is evaluated until one returns a true
-result = isColorRed("Red") || isGreaterThan1400(200);
-console.log("");
-console.log("Calling isGreaterThan1400() first");
-}
-shortCircuitOrSample();
+// function throwError()
+// {
+// try{
+// attemptDivision();
+// }
+// catch(error)
+// {
+// console.log(error.message+ "-Error Type:" +error.name);
+// }
+// }
+// function attemptDivision()
+// {
+// let result;
+// try{
+// result=x/10;
+// }
+// catch(error)
+// {
+// //Always include at least a 'message' and 'name' properties
+// throw{
+// "message":"In the attemptDivision() method the following error occured:"+error.message,
+// "name":"CustomError"
+// };
+// }
+// }
+// throwError();
