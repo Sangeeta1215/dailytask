7bf56653937750663fa01629d2437e6dda870669
Sangeeta Devi <sd705194@gmail.com>
2023-05-01 17:32:07 +0530

    initial

--- 05/demos/after/js-demo/obj.js
+++ 05/demos/after/js-demo/obj.js
@@ -1,46 +1,46 @@
-function tryCatch()
+// function tryCatch()
+// {
+// let result;
+// try
+// {
+// console.log("an error will ocuur");
+// result=x/9;
+// console.log("an error is occured");
+// }
+// catch(error)
+// {
+// console.log(error.message);             //a javascript error object always has "name" and "message" properties.
+// }
+// finally
+// {
+// console.log("This block always executes");
+// }
+// }
+// tryCatch();
+
+function throwError()
 {
-let result;
-try
-{
-console.log("an error will ocuur");
-result=x/9;
-console.log("an error is occured");
+try{
+attemptDivision();
 }
 catch(error)
 {
-console.log(error.message);             //a javascript error object always has "name" and "message" properties.
+console.log(error.message+ "-Error Type:" +error.name);
 }
-finally
+}
+function attemptDivision()
 {
-console.log("This block always executes");
+let result;
+try{
+result=x/10;
+}
+catch(error)
+{
+//Always include at least a 'message' and 'name' properties
+throw{
+"message":"In the attemptDivision() method the following error occured:"+error.message,
+"name":"CustomError"
+};
 }
 }
-tryCatch();
-
-// function throwError()
-// {
-// try{
-// attemptDivision();
-// }
-// catch(error)
-// {
-// console.log(error.message+ "-Error Type:" +error.name);
-// }
-// }
-// function attemptDivision()
-// {
-// let result;
-// try{
-// result=x/10;
-// }
-// catch(error)
-// {
-// //Always include at least a 'message' and 'name' properties
-// throw{
-// "message":"In the attemptDivision() method the following error occured:"+error.message,
-// "name":"CustomError"
-// };
-// }
-// }
-// throwError();
+throwError();
