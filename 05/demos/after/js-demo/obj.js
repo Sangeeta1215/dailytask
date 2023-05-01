824a495cbd160ed193713f3c78d2dd6ac0f077e9
Sangeeta Devi <sd705194@gmail.com>
2023-05-01 15:40:58 +0530

    initial

--- 05/demos/after/js-demo/obj.js
+++ 05/demos/after/js-demo/obj.js
@@ -1,3 +1,11 @@
 'use strict'
-result=10;
+result=10;                 //Can't use a variable without var or let
 console.log(result);
+
+//Can't use reserved words as variables
+let eval=10;
+let arguments="some args";
+//Can't delete a variable
+delete result;
+//Can't delete a function
+delete useStrictSample;
