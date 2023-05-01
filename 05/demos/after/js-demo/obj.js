b271464a8d691ab450dd9ff4401587a06bb04f83
Sangeeta Devi <sd705194@gmail.com>
2023-05-01 15:19:02 +0530

    initial

--- 05/demos/after/js-demo/obj.js
+++ 05/demos/after/js-demo/obj.js
@@ -1,71 +1,19 @@
-var a=
-[
-{
-name:"Rahul",
-age:63
-},
-{
-    name:"Ram",
-    age:7
-    },
-    {
-        name:"Rajan",
-        age:2
-        }
-];
+let price=10;
+let result=0;
+console.log("price="+price.toString());
+result=price+100;
+console.log("price + 100 ="+result.toString());
+result=price-100;
+console.log("price-100="+result.toString());
+result=result*2;
+console.log("price*2="+result.toString());
+result=result/2;
+console.log("price/2="+result.toString());
+result=result**2;
+console.log("price**2="+result.toString());
+result=result%2;
+console.log("price%2="+result.toString());
 
-function fun(a){
-for(var item of a)
-{
-if(item.age>10)
-{
-    continue;
-}
-console.log(item);
-}
-}
-fun(a);
 
-// let products=[
-//     {
-//         productId:680,
-//         name:"HL Road Frame - Black,58",
-//         standardCost:1059.31
-//     },
-//     {
-//         productId:707,
-//         name:"HL Road Frame - Black,58",
-//         standardCost:13.08
-//     },
-//     {
-//         productId:980,
-//         name:"HL Road Frame - Black,58",
-//         standardCost:9.50
-//     }
-// ];
-// function fun()
-// {
-// for(const item of products)
-// {
-// if(item.standardCost<20)
-// {
-// break;
-// }
-// console.log(item);
-// }
-// }
-// fun();
 
-// function labelSample()
-// {
-// even:
-// for(let index=1;index<=10;index++)
-// {
-// if(index %2==1)
-// {
-// continue even;
-// }
-// console.log(index);
-// }
-// }
-// labelSample();
+
