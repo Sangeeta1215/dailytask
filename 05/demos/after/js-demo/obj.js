973851b74f8672fd0894f3a2f28e152da69436dc
Sangeeta Devi <sd705194@gmail.com>
2023-05-01 08:25:11 +0530

    initial

--- 05/demos/after/js-demo/obj.js
+++ 05/demos/after/js-demo/obj.js
@@ -26,46 +26,46 @@
 // }
 // fun(a);
 
-// let products=[
-//     {
-//         productId:680,
-//         name:"HL Road Frame - Black,58",
-//         standardCost:1059.31
-//     },
-//     {
-//         productId:707,
-//         name:"HL Road Frame - Black,58",
-//         standardCost:13.08
-//     },
-//     {
-//         productId:980,
-//         name:"HL Road Frame - Black,58",
-//         standardCost:9.50
-//     }
-// ];
-// function fun()
-// {
-// for(const item of products)
-// {
-// if(item.standardCost<20)
-// {
-// break;
-// }
-// console.log(item);
-// }
-// }
-// fun();
+let products=[
+    {
+        productId:680,
+        name:"HL Road Frame - Black,58",
+        standardCost:1059.31
+    },
+    {
+        productId:707,
+        name:"HL Road Frame - Black,58",
+        standardCost:13.08
+    },
+    {
+        productId:980,
+        name:"HL Road Frame - Black,58",
+        standardCost:9.50
+    }
+];
+function fun()
+{
+for(const item of products)
+{
+if(item.standardCost<20)
+{
+break;
+}
+console.log(item);
+}
+}
+fun();
 
-function labelSample()
-{
-even:
-for(let index=1;index<=10;index++)
-{
-if(index %2==1)
-{
-continue even;
-}
-console.log(index);
-}
-}
-labelSample();
+// function labelSample()
+// {
+// even:
+// for(let index=1;index<=10;index++)
+// {
+// if(index %2==1)
+// {
+// continue even;
+// }
+// console.log(index);
+// }
+// }
+// labelSample();
