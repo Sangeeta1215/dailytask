0b5f3c11182cb1d71931517e2e278254e1e86b5e
Sangeeta Devi <sd705194@gmail.com>
2023-05-01 15:34:59 +0530

    initial

--- 05/demos/after/js-demo/obj.js
+++ 05/demos/after/js-demo/obj.js
@@ -1,15 +1,3 @@
-let price=200;
-let stringValue="100";
-let result=0;
-console.log("price =" +price.toString());
-
-result=price+stringValue;
-console.log("result(number + stringValue)="+result.toString());
-
-result=price+(+stringValue);
-console.log("result(number +(+stringValue))="+result.toString());
-
-let i=10;
-let j=4;
-j+=i;
-console.log(j)
+//'use strict'
+result=10;
+console.log(result);
